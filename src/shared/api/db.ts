c0f1120ebe8cd376b330544b0df9cc0413be49ca
hetarho <sunlikeperson@gmail.com>
2025-10-22 20:26:12 +0900

    fix: update import path for Firebase configuration in db.ts

--- src/shared/api/db.ts
+++ src/shared/api/db.ts
@@ -1,5 +1,5 @@
 import { getFirestore } from "firebase/firestore";
-import { app } from "./config/firebase";
+import { app } from "../lib/firebase/config";
 
 // Initialize Cloud Firestore and get a reference to the service
 export const db = getFirestore(app);
